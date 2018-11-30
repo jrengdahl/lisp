fb96415e7651431fa5e9d506e603a0ec5ebb1e95
Jonathan Engdahl <engdahl@windstream.net>
2018-11-29 19:43:28 -0500

    rename test.cpp to lisp.cpp

--- new/lisp.cpp
+++ new/lisp.cpp
@@ -1,79 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 #include "lispdefs.h"
 
+extern void init_symbols();
 
-
-
-#if 0
-
-int exit()
+int main(int argc, char **argv)
     {
-    printf("; exiting lisp\n");
+    init_symbols();
+    lisp_read();
 
-    // cleanup memory system is needed
-    // shutdown
-
-    return 1;
-    }
-
-
-void error()
-    {
-    printf("; serious error: restarting\n");
-
-    // longjmp to restart
-    }
-
-
-void stackovfl()
-    {
-    printf("; stack overflow: restarting\n");
-
-    // longjmp to restart
-    }
-
-#endif
-
-
-void init_symbols();
-
-int main()
-    {
-
-
-//    startup();
-
-    printf("; (lisp) 2.0 Copyright (c) 1987, 2018 by Jonathan Engdahl\n");
-
-//    init_memory();
-      init_symbols();
-//    init_specials();
-//    init_lists();
-//    init_functions();
-//    init_evaluator();
-//    init_reader();
-//    init_printer();
-//    init_booleans();
-//    init_conditionals();
-//    init_numbers();
-//    init_files();
-
-    // setjmp restart
-
-    // primitive exit jumps to here
-    // primitive error jumps to here
-
-
-    while(true)
-        {
-        node *list;
-
-        list = lisp_read();
-//        list = eval(list);
-//        if(verbose)
-//            {
-//            lisp_print(list);
-//            }
-        }
+    return 0;
     }
